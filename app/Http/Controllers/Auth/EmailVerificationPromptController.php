3b962d97cfcc4414cbf8116ae9d7b78074cd873f
Valdir Júnior <valdirmsjunior@gmail.com>
2024-05-24 17:38:29 -0300

    primeiro commit

---
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\{RedirectResponse, Request};
+use Inertia\{Inertia, Response};
+
+class EmailVerificationPromptController extends Controller
+{
+    /**
+     * Display the email verification prompt.
+     */
+    public function __invoke(Request $request): RedirectResponse|Response
+    {
+        return $request->user()->hasVerifiedEmail()
+                    ? redirect()->intended(RouteServiceProvider::HOME)
+                    : Inertia::render('Auth/VerifyEmail', ['status' => session('status')]);
+    }
+}
