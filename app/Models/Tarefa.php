71011cd788122fde826b3e495e58c53279ac45be
Valdir Júnior <valdirmsjunior@gmail.com>
2024-05-25 21:48:18 -0300

    alterar rota e model tarefa

--- app/Models/Tarefa.php
+++ app/Models/Tarefa.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Tarefa extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nome', 'prioridade', 'status', 'user_id', 'vencimento'
+    ];
+
+    //Adicionando os enums
+    public const PRIORIDADE_ALTA = 'alta';
+    public const PRIORIDADE_MEDIA = 'media';
+    public const PRIORIDADE_BAIXA = 'baixa';
+
+    public const PRIORIDADES = [
+        self::PRIORIDADE_ALTA,
+        self::PRIORIDADE_MEDIA,
+        self::PRIORIDADE_BAIXA,
+    ];
+
+    public const STATUS_NAO_INICIADO = 'Não iniciado';
+    public const STATUS_EM_PROGRESSO = 'Em progresso';
+    public const STATUS_CONCLUIDO = 'Concluído';
+
+    public const STATUSES = [
+        self::STATUS_NAO_INICIADO,
+        self::STATUS_EM_PROGRESSO,
+        self::STATUS_CONCLUIDO,
+    ];
 }
