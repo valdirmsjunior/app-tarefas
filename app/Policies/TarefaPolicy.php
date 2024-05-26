99ff2b70b0db72a007e0351a3c6ac15dfd631ea0
Valdir Júnior <valdirmsjunior@gmail.com>
2024-05-25 22:23:56 -0300

    alterando o policy

--- app/Policies/TarefaPolicy.php
+++ app/Policies/TarefaPolicy.php
@@ -6,11 +6,23 @@ use App\Models\User;
 
 class TarefaPolicy
 {
-    /**
-     * Create a new policy instance.
-     */
-    public function __construct()
+    public function viewAny(?User $user): bool
     {
-        //
+        return $user->id == Auth::user()->id;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id;
+    }
+
+    public function delete(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id;
     }
 }
